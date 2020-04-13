6be0ebcabd9520ccf4c69822bfd72e405c75fb54
dima <dimak@voyagerlabs.co>
2020-04-13 10:16:10 +0300

    added f1

--- src/part1/Test1.java
+++ src/part1/Test1.java
@@ -2,9 +2,16 @@ package part1;
 
 public class Test1 {
 
+    public void f1(){
+
+    }
+
     public static void main(String[] args) {
         System.out.println("hello world");
 
         System.out.println("more print");
+
+
+
     }
 }
